5e7ee5a6373dc22d2d2db28c3075dcff22ff0d1e
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-05-15 16:16:16 +0200

    Updated title

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Audio Track',
+      title: 'Unit Converter',
       home: Scaffold(
         backgroundColor: Colors.green[100],
         body: Center(
